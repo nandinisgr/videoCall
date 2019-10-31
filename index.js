3895801e27aa731bf206af74643bd8e1ed559ca4
nandinisgr <41414319+nandinisgr@users.noreply.github.com>
2019-10-31 18:10:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+const app = express();
+
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Session middleware
+
+// Create an instance of Pusher
+const pusher = new Pusher({
+    appId: '890054',
+    key: 'c69e1f45d921525d41b6',
+    secret: 'da0bf8a14f6a1386d242',
+    cluster: 'ap2',
+    encrypted: true
+});
+
+
+app.get('/', (req, res) => {
+    return res.sendFile(__dirname + '/index.html');
+});
+
+// get authentictation for the channel;
+app.post('/pusher/auth', (req, res) => {
+        const socketId = req.body.socket_id;
+        const channel = req.body.channel_name;
+        var presenceData = {
+            user_id: Math.random().toString(36).slice(2) + Date.now()
+        }
+        const auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+});
+
+//listen on the app
+app.listen(3000, () => {
+    return console.log('Server is up on 3000')
+});
